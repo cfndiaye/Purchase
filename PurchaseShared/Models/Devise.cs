441e55c2dad27c31e77b547c2360b510b8e1ceb0
Cheikh faye ndiaye <cfndiaye@gmail.com>
2022-05-18 22:21:34 +0000

    update devise on order

---
+++ PurchaseShared/Models/Devise.cs
@@ -0,0 +1,12 @@
+using System;
+namespace PurchaseShared.Models
+{
+    public static class Devise
+    {
+        public static string XOF = "XOF";
+        public static string EUR = "EUR";
+        public static string USD = "USD";
+        public static string GBP = "GBP";
+
+    }
+}
