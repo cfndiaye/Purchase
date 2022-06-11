258e0e6e04e494ab0e53d99b22236bc49b86093b
cfndiaye <cfndiaye@outlook.com>
2022-06-11 21:05:14 +0000

    Adding Serilog

--- Purchase/Program.cs
+++ Purchase/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Purchase
 {
@@ -13,20 +14,40 @@ namespace Purchase
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //Read configuration builder
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            //Initialize logger
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+            try
+            {
+                Log.Information("Application starting");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "The Application failed to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureLogging(logBuilder => {
+            .UseSerilog() //Use Serilog instead of default .Net Logger
+            /*.ConfigureLogging(logBuilder => {
                 logBuilder.ClearProviders(); // removes all providers from LoggerFactory
                 logBuilder.AddConsole();
                 logBuilder.AddTraceSource("Information, ActivityTracing"); // Add Trace listener provider
 
-            })
+            })*/
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+                
     }
 }
