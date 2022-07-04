3936df11a81aaf4f1b9e93943fa14068b72937bb
cfndiaye <cfndiaye@DESKTOP-H221PTS>
2022-07-04 16:36:28 +0000

    Mongodb connect moved to Atlas

--- Purchase.Infrastructure/Models/MongoContext.cs
+++ Purchase.Infrastructure/Models/MongoContext.cs
@@ -12,15 +12,17 @@ namespace Purchase.Infrastructure.Models
                 purchaseOption.Value.User,
                 purchaseOption.Value.Password
                 );
-            
-            _mongoClientSettings = new MongoClientSettings
-            {
-                Server = _serverAddress,
-                Credential = _credential,
-                MaxConnectionPoolSize = 1000,
-                WaitQueueSize = 20000
-            };
-            _mongoClient = new MongoClient(_mongoClientSettings);
+
+            //_mongoClientSettings = new MongoClientSettings
+            //{
+            //    Server = _serverAddress,
+            //    Credential = _credential,
+            //    MaxConnectionPoolSize = 1000,
+            //    WaitQueueSize = 20000
+            //};
+
+            _mongoConnectionString = purchaseOption.Value.ConnectionString;
+            _mongoClient = new MongoClient(_mongoConnectionString);
             _mongoDatabase = _mongoClient.GetDatabase(purchaseOption.Value.DatabaseName);
 
         }
@@ -28,7 +30,8 @@ namespace Purchase.Infrastructure.Models
         protected readonly MongoClient _mongoClient;
         protected readonly MongoCredential _credential;
         protected readonly MongoServerAddress _serverAddress;
-        protected readonly MongoClientSettings _mongoClientSettings;
+        // protected readonly MongoClientSettings _mongoClientSettings;
+        protected readonly string _mongoConnectionString;
         protected readonly IMongoDatabase _mongoDatabase;
 
         public IMongoDatabase GetDatabase()
