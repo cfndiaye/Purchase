a0ef2138750a1e77b61d073a6e0f69f021f2cca4
cfndiaye <cfndiaye@DESKTOP-H221PTS>
2022-07-04 15:37:30 +0000

    Change to mongoDb atlas and add purchaseresquest Model

---
+++ PurchaseShared/Models/PurchaseRequestLine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchaseShared.Models
+{
+    public class PurchaseRequestLine
+    {
+        public int RequestNumber { get; set; }
+        public int LineNumber { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemCoswinCode { get; set; }
+        public string ITemDescription { get; set; }
+        public string ITemEnglishDescription { get; set; }
+        public string Unit { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string Location { get; set; }
+        public string Status { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime RequestApprovedDate { get; set; }
+        public DateTime RequestValidationMMDDate { get; set; }
+        public string Requestor { get; set; }
+        public string RequestDescription { get; set; }
+        public string LastPurchaseOrder { get; set; }
+        public DateTime LastPurchaseOrderDate { get; set; }
+        public string LastPoVendor { get; set; }
+        public string LastPurchaseCurrency { get; set; }
+        public decimal LastPurchaseUnitePrice { get; set; }
+
+
+    }
+}
