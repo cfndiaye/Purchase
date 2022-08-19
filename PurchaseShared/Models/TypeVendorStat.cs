e69263271ff57ab242ca31da57f7b481b33cf888
cfndiaye <cfndiaye@outlook.com>
2022-08-19 00:20:06 +0000

    chart widgets added Radzen

---
+++ PurchaseShared/Models/TypeVendorStat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PurchaseShared.Models
+{
+    public class TypeVendorStat
+    {
+        public TypeVendorStat(string vendorType, decimal value)
+        {
+            VendorType = vendorType;
+            Value = value;
+        }
+        public string VendorType { get; set; }
+        public decimal Value { get; set; }
+    }
+}
+
